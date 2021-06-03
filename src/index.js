2f30b3c4af48e519ff785e4463c4dd73d4c1c638
Oscar Eduardo Montes <oscar.montes@tj.local>
2021-06-03 17:46:16 -0500

    Instalacion de nodemon seguir viendo video de creacion de api con mysql

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const port = 3000;
 
-app.listen(3000, () => {
-    console.log('Server on port ${3000}')
+// middlewares
+app.use(morgan('dev'));
+
+// Rutas
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+// Starting the server
+app.listen(port, () => {
+    console.log('Server on port ${3000}');
 })
