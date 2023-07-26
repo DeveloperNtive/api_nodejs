82b8d93fb52ed7a444a77bd8dca1803b609d4268
Oscar Eduardo Montes Perez <omontes@enigmadev.com>
2023-07-26 15:22:44 -0500

    cambios db

--- src/DB/mongo.js
+++ src/DB/mongo.js
@@ -1,6 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const uri =
-  'mongodb+srv://aniki:do2dnxwvWNieYyiX@cluster0.iazji.mongodb.net/nodejsAPI?retryWrites=true&w=majority'
+"mongodb+srv://aniki:do2dnxwvWNieYyiX@cluster0.iazji.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
